33b8e16ef613a242c27c00d149da6e88d5e00636
Culyc <84857830+Culyc@users.noreply.github.com>
2022-03-19 09:33:02 +0400

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,241 @@
+(() => {
+  "use strict";
+  function e(e) {
+    this.type = e;
+  }
+  (e.prototype.init = function () {
+    const e = this;
+    (this.оbjects = []),
+      (this.daClassname = "_dynamic_adapt_"),
+      (this.nodes = document.querySelectorAll("[data-da]"));
+    for (let e = 0; e < this.nodes.length; e++) {
+      const t = this.nodes[e],
+        n = t.dataset.da.trim().split(","),
+        i = {};
+      (i.element = t),
+        (i.parent = t.parentNode),
+        (i.destination = document.querySelector(n[0].trim())),
+        (i.breakpoint = n[1] ? n[1].trim() : "767"),
+        (i.place = n[2] ? n[2].trim() : "last"),
+        (i.index = this.indexInParent(i.parent, i.element)),
+        this.оbjects.push(i);
+    }
+    this.arraySort(this.оbjects),
+      (this.mediaQueries = Array.prototype.map.call(
+        this.оbjects,
+        function (e) {
+          return (
+            "(" + this.type + "-width: " + e.breakpoint + "px)," + e.breakpoint
+          );
+        },
+        this
+      )),
+      (this.mediaQueries = Array.prototype.filter.call(
+        this.mediaQueries,
+        function (e, t, n) {
+          return Array.prototype.indexOf.call(n, e) === t;
+        }
+      ));
+    for (let t = 0; t < this.mediaQueries.length; t++) {
+      const n = this.mediaQueries[t],
+        i = String.prototype.split.call(n, ","),
+        o = window.matchMedia(i[0]),
+        a = i[1],
+        r = Array.prototype.filter.call(this.оbjects, function (e) {
+          return e.breakpoint === a;
+        });
+      o.addListener(function () {
+        e.mediaHandler(o, r);
+      }),
+        this.mediaHandler(o, r);
+    }
+  }),
+    (e.prototype.mediaHandler = function (e, t) {
+      if (e.matches)
+        for (let e = 0; e < t.length; e++) {
+          const n = t[e];
+          (n.index = this.indexInParent(n.parent, n.element)),
+            this.moveTo(n.place, n.element, n.destination);
+        }
+      else
+        for (let e = t.length - 1; e >= 0; e--) {
+          const n = t[e];
+          n.element.classList.contains(this.daClassname) &&
+            this.moveBack(n.parent, n.element, n.index);
+        }
+    }),
+    (e.prototype.moveTo = function (e, t, n) {
+      t.classList.add(this.daClassname),
+        "last" === e || e >= n.children.length
+          ? n.insertAdjacentElement("beforeend", t)
+          : "first" !== e
+          ? n.children[e].insertAdjacentElement("beforebegin", t)
+          : n.insertAdjacentElement("afterbegin", t);
+    }),
+    (e.prototype.moveBack = function (e, t, n) {
+      t.classList.remove(this.daClassname),
+        void 0 !== e.children[n]
+          ? e.children[n].insertAdjacentElement("beforebegin", t)
+          : e.insertAdjacentElement("beforeend", t);
+    }),
+    (e.prototype.indexInParent = function (e, t) {
+      const n = Array.prototype.slice.call(e.children);
+      return Array.prototype.indexOf.call(n, t);
+    }),
+    (e.prototype.arraySort = function (e) {
+      "min" === this.type
+        ? Array.prototype.sort.call(e, function (e, t) {
+            return e.breakpoint === t.breakpoint
+              ? e.place === t.place
+                ? 0
+                : "first" === e.place || "last" === t.place
+                ? -1
+                : "last" === e.place || "first" === t.place
+                ? 1
+                : e.place - t.place
+              : e.breakpoint - t.breakpoint;
+          })
+        : Array.prototype.sort.call(e, function (e, t) {
+            return e.breakpoint === t.breakpoint
+              ? e.place === t.place
+                ? 0
+                : "first" === e.place || "last" === t.place
+                ? 1
+                : "last" === e.place || "first" === t.place
+                ? -1
+                : t.place - e.place
+              : t.breakpoint - e.breakpoint;
+          });
+    });
+  new e("max").init();
+  class t {
+    constructor(e, t = null) {
+      if (
+        ((this.config = Object.assign({ init: !0, logging: !0 }, e)),
+        this.config.init)
+      ) {
+        const e = document.querySelectorAll("[data-prlx-mouse]");
+        e.length
+          ? (this.paralaxMouseInit(e),
+            this.setLogging(`Проснулся, слежу за объектами: (${e.length})`))
+          : this.setLogging("Нет ни одного объекта. Сплю...zzZZZzZZz...");
+      }
+    }
+    paralaxMouseInit(e) {
+      e.forEach((e) => {
+        const t = e.closest("[data-prlx-mouse-wrapper]"),
+          n = e.dataset.prlxCx ? +e.dataset.prlxCx : 100,
+          i = e.dataset.prlxCy ? +e.dataset.prlxCy : 100,
+          o = e.hasAttribute("data-prlx-dxr") ? -1 : 1,
+          a = e.hasAttribute("data-prlx-dyr") ? -1 : 1,
+          r = e.dataset.prlxA ? +e.dataset.prlxA : 50;
+        let l = 0,
+          s = 0,
+          c = 0,
+          d = 0;
+        function p(t = window) {
+          t.addEventListener("mousemove", function (t) {
+            const n = e.getBoundingClientRect().top + window.scrollY;
+            if (n >= window.scrollY || n + e.offsetHeight >= window.scrollY) {
+              const e = window.innerWidth,
+                n = window.innerHeight,
+                i = t.clientX - e / 2,
+                o = t.clientY - n / 2;
+              (c = (i / e) * 100), (d = (o / n) * 100);
+            }
+          });
+        }
+        !(function t() {
+          (l += ((c - l) * r) / 1e3),
+            (s += ((d - s) * r) / 1e3),
+            (e.style.cssText = `transform: translate3D(${(o * l) / (n / 10)}%,${
+              (a * s) / (i / 10)
+            }%,0);`),
+            requestAnimationFrame(t);
+        })(),
+          t ? p(t) : p();
+      });
+    }
+    setLogging(e) {
+      this.config.logging &&
+        (function (e) {
+          setTimeout(() => {
+            window.FLS && console.log(e);
+          }, 0);
+        })(`[PRLX Mouse]: ${e}`);
+    }
+  }
+  let n = !0,
+    i = (e = 500) => {
+      let t = document.querySelector("body");
+      if (n) {
+        let i = document.querySelectorAll("[data-lp]");
+        setTimeout(() => {
+          for (let e = 0; e < i.length; e++) {
+            i[e].style.paddingRight = "0px";
+          }
+          (t.style.paddingRight = "0px"),
+            document.documentElement.classList.remove("lock");
+        }, e),
+          (n = !1),
+          setTimeout(function () {
+            n = !0;
+          }, e);
+      }
+    },
+    o = (e = 500) => {
+      let t = document.querySelector("body");
+      if (n) {
+        let i = document.querySelectorAll("[data-lp]");
+        for (let e = 0; e < i.length; e++) {
+          i[e].style.paddingRight =
+            window.innerWidth -
+            document.querySelector(".wrapper").offsetWidth +
+            "px";
+        }
+        (t.style.paddingRight =
+          window.innerWidth -
+          document.querySelector(".wrapper").offsetWidth +
+          "px"),
+          document.documentElement.classList.add("lock"),
+          (n = !1),
+          setTimeout(function () {
+            n = !0;
+          }, e);
+      }
+    };
+  let a = !1;
+  setTimeout(() => {
+    if (a) {
+      let e = new Event("windowScroll");
+      window.addEventListener("scroll", function (t) {
+        document.dispatchEvent(e);
+      });
+    }
+  }, 0),
+    (window.FLS = !0),
+    (function (e) {
+      let t = new Image();
+      (t.onload = t.onerror =
+        function () {
+          e(2 == t.height);
+        }),
+        (t.src =
+          "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA");
+    })(function (e) {
+      let t = !0 === e ? "webp" : "no-webp";
+      document.documentElement.classList.add(t);
+    }),
+    (function () {
+      let e = document.querySelector(".icon-menu");
+      e &&
+        e.addEventListener("click", function (e) {
+          n &&
+            (((e = 500) => {
+              document.documentElement.classList.contains("lock") ? i(e) : o(e);
+            })(),
+            document.documentElement.classList.toggle("menu-open"));
+        });
+    })(),
+    new t({});
+})();
